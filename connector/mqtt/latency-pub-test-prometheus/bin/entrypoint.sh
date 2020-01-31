bd4acba2977f386dc1e3ce335cf6f8c0b4102cd0
Mariane Previde <mprevide@cpqd.com.br>
2020-01-31 15:51:47 -0300

    Update entrypoint.sh

--- connector/mqtt/latency-pub-test-prometheus/bin/entrypoint.sh
+++ connector/mqtt/latency-pub-test-prometheus/bin/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+#
+# command.sh
+#
+# The Dockerfile CMD, or any "docker run" command option, gets
+# passed as command-line arguments to this script.
 
-# set -ex
+# Abort on any error (good shell hygiene)
+set -e
 
 npm run run-client
