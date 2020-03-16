e43097f375efb13aee07015aae2d1bb14153930a
Mariane Previde <mprevide@cpqd.com.br>
2020-03-16 09:19:33 -0300

    improve doc

--- util/microservice-sdk/js/examples/sample.js
+++ util/microservice-sdk/js/examples/sample.js
@@ -6,7 +6,7 @@ const TAG = { filename: 'sample' };
 const config = {
     // Kafka's consumer group id
     "group.id": process.env.KAFKA_GROUP_ID || "sdk-exampĺe",
-    // Address of the kafka broker
+    // Addresses of the kafka brokers separated by a comma
     "metadata.broker.list": process.env.KAFKA_HOSTS || "kafka:9092",
 };
 
