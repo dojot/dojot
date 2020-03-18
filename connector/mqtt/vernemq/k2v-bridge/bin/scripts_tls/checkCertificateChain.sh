880156fb3ea16fb92e9439601ac0c24f6af6387f
Jonaphael Aristil <jonaphael4@gmail.com>
2020-03-18 11:21:10 -0300

    feat: [mqtt/vernemq/k2v-bridge] add k2v-bridge service (#1375)
    * added k2v bridge service
    
    * Fixed geral comments on k2v-bridge
    
    * readme update and mqqt client tls fix
    
    * fixed comments and doc
    
    * fixed comments
    
    * fixed doc and type

---
+++ connector/mqtt/vernemq/k2v-bridge/bin/scripts_tls/checkCertificateChain.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#########################################################
+
+#########################################################
+
+BASE_DIR=${BASE_DIR:-"/k2v_bridge"}
+
+. "${BASE_DIR}"/bin/scripts_tls/_initVariables.sh
+
+if openssl verify -CAfile "${certDir}"/"${certCaFile}" "${certDir}"/"${certCertFile}" 
+then 
+    echo "Certificate Chain - Verification OK"
+else
+    echo "Certificate Chain - Verification failed"
+   
+    exit 2
+fi
---
+++ connector/mqtt/vernemq/k2v-bridge/test/unit/mqttClient.test.js
@@ -0,0 +1,154 @@
+/**
+ * Unit test for MqttClient file
+ *
+ * This module has the following dependencies
+ *
+ * - mqtt
+ * - fs
+ */
+
+const mqtt = require('mqtt');
+const defaultConfig = require('../../app/config');
+const MqttClient = require('../../app/MqttClient');
+
+/* dependencies mock */
+const mockMqtt = {
+  on: jest.fn(),
+  reconnect: jest.fn(),
+  publish: jest.fn(),
+};
+
+jest.mock('mqtt', () => ({
+  connect: jest.fn(() => mockMqtt),
+  disconnect: jest.fn(),
+}));
+
+jest.mock('../../app/AgentMessenger', () => jest.fn(() => ({
+  init: jest.fn(),
+})));
+
+const mockDefaultConfig = {
+  mqtt: {
+    clientUsername: 'fake',
+    clientId: 'fake',
+    host: 'mqtt-host',
+    port: 0,
+    keepAlive: 0,
+    secure: true,
+    publishTopicSuffix: '/fake',
+    publishQos: 0,
+    tls: {
+      ca: {
+        location: 'fake',
+      },
+      certificate: {
+        location: 'fake',
+      },
+      privateKey: {
+        location: 'fake',
+      },
+    },
+  },
+  app: {
+    mqtt_log_level: 'debug',
+    baseDir: 'fakeDir',
+    hostname: 'fake',
+  },
+  toBoolean: (val) => val,
+};
+
+
+jest.mock('fs');
+jest.mock('../../app/utils/utils');
+jest.mock('@dojot/dojot-module-logger');
+
+describe('Testing MqttClient', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const expectMqttInitialization = (client, config = defaultConfig) => {
+    expect(client.config).toEqual(config);
+    expect(client.isConnected).toEqual(false);
+
+    expect(client.clientId).toEqual(config.mqtt.clientId);
+    expect(client.username).toEqual(config.mqtt.clientUsername);
+    expect(client.host).toEqual(config.mqtt.host);
+    expect(client.keepAlive).toEqual(config.mqtt.keepAlive);
+
+    expect(client.privateKey).not.toBeNull();
+    expect(client.clientCrt).not.toBeNull();
+    expect(client.ca).not.toBeNull();
+  };
+
+  it('Should initialize the app correctly with default config', () => {
+    const mqttClient = new MqttClient();
+    expectMqttInitialization(mqttClient);
+  });
+
+  it('Should initialize the app correctly with mocked configs', () => {
+    const mqttClient = new MqttClient(mockDefaultConfig);
+    expectMqttInitialization(mqttClient, mockDefaultConfig);
+  });
+
+  it('Should initializing correctly the client with protocol mqtt', () => {
+    mockDefaultConfig.mqtt.secure = false;
+    const mqttClient = new MqttClient(mockDefaultConfig);
+    mqttClient.init();
+
+    expect(mqtt.connect).toHaveBeenCalledTimes(1);
+    expect(mockMqtt.on).toHaveBeenCalledTimes(2);
+  });
+
+  it('Should initializing correctly the client with protocol mqtts', () => {
+    mockDefaultConfig.mqtt.secure = true;
+    const mqttClient = new MqttClient(mockDefaultConfig);
+    mqttClient.init();
+
+    expect(mqtt.connect).toHaveBeenCalledTimes(1);
+    expect(mockMqtt.on).toHaveBeenCalledTimes(2);
+  });
+
+  it('should connect successfully the client (callback)', () => {
+    const mqttClient = new MqttClient(mockDefaultConfig);
+    mqttClient.init();
+    const spyAgentMessengerInit = jest.spyOn(mqttClient.agentMessenger, 'init');
+
+    mqttClient.onConnect();
+
+    expect(mqttClient.isConnected).toEqual(true);
+    expect(spyAgentMessengerInit).toHaveBeenCalledTimes(1);
+  });
+
+
+  it('should disconnect and reconnect successfully the client (callback)', () => {
+    const mqttClient = new MqttClient(mockDefaultConfig);
+    mqttClient.init();
+
+    mqttClient.onDisconnect();
+
+    expect(mqttClient.isConnected).toEqual(false);
+    expect(mockMqtt.reconnect).toHaveBeenCalledTimes(1);
+  });
+
+  it('should publish a message', () => {
+    const mqttClient = new MqttClient(mockDefaultConfig);
+    mqttClient.init();
+    mqttClient.onConnect();
+    const fakeTopic = 'fake-topic';
+    const fakeMessage = { key: 'value' };
+
+    mqttClient.publishMessage(fakeTopic, fakeMessage);
+
+    expect(mockMqtt.publish).toHaveBeenCalledWith(fakeTopic,
+      fakeMessage,
+      { qos: mockDefaultConfig.mqtt.publishQos });
+  });
+
+  it('should not publish a message', () => {
+    const mqttClient = new MqttClient(mockDefaultConfig);
+    mqttClient.init();
+    mqttClient.publishMessage();
+    expect(mockMqtt.publish).not.toHaveBeenCalled();
+  });
+});
