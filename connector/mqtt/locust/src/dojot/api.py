aeedaee0b1bca7597020e3a2986f5dd2de4dcb55
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2020-03-24 14:05:39 -0300

    Add retries to HTTP calls in DojotAPI class

--- connector/mqtt/locust/src/dojot/api.py
+++ connector/mqtt/locust/src/dojot/api.py
@@ -2,13 +2,21 @@
 API calls to Dojot.
 """
 import json
+from typing import Callable, List, Dict
 import requests
+import gevent
 
 from src.config import CONFIG
 from src.utils import Utils
 
 
 LOGGER = Utils.create_logger("api")
+
+
+class APICallError(Exception):
+    """
+    Error when trying to call Dojot API.
+    """
 
 
 class DojotAPI():
@@ -20,25 +28,26 @@ class DojotAPI():
         """
         Request a JWT token.
         """
-        LOGGER.info("Retrieving JWT...")
+        LOGGER.debug("Retrieving JWT...")
 
-        data = {
-            "username": CONFIG['dojot']['user'],
-            "passwd": CONFIG['dojot']['passwd'],
+        args = {
+            "url": "{0}/auth".format(CONFIG['dojot']['url']),
+            "data": json.dumps({
+                "username": CONFIG['dojot']['user'],
+                "passwd": CONFIG['dojot']['passwd'],
+            }),
+            "headers": {
+                "Content-Type": "application/json"
+            },
         }
 
-        res = requests.post(
-            "{0}/auth".format(CONFIG['dojot']['url']),
-            data=json.dumps(data),
-            headers={"Content-Type": "application/json"},
-        )
-        res.raise_for_status()
+        res = DojotAPI.call_api(requests.post, args)
 
-        LOGGER.info(".. retrieved JWT")
-        return json.loads(res.text)["jwt"]
+        LOGGER.debug(".. retrieved JWT")
+        return res["jwt"]
 
     @staticmethod
-    def create_devices(jwt: str, template_id: str, n: int, batch: int) -> None:
+    def create_devices(jwt: str, template_id: str, total: int, batch: int) -> None:
         """
         Create the devices.
 
@@ -48,31 +57,28 @@ class DojotAPI():
             n: total number of devices to be created
             batch: number of devices to be created in each iteration
         """
-        LOGGER.info("Creating devices...")
+        LOGGER.debug("Creating devices...")
 
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer {0}".format(jwt),
+        args = {
+            "headers": {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {0}".format(jwt),
+            },
         }
 
-        loads = DojotAPI.divide_loads(n, batch)
+        loads = DojotAPI.divide_loads(total, batch)
 
         for i, load in enumerate(loads):
-            device = {
+            args["data"] = json.dumps({
                 "templates": [template_id],
                 "attrs": {},
                 "label": "CargoContainer_{0}".format(i)
-            }
+            })
+            args["url"] = "{0}/device?count={1}&verbose=false".format(CONFIG['dojot']['url'], load)
 
-            res = requests.post(
-                "{0}/device?count={1}&verbose=false".format(CONFIG['dojot']['url'], load),
-                data=json.dumps(device),
-                headers=headers
-            )
+            DojotAPI.call_api(requests.post, args)
 
-            res.raise_for_status()
-
-        LOGGER.info("... created the devices")
+        LOGGER.debug("... created the devices")
 
     @staticmethod
     def create_template(jwt: str) -> str:
@@ -81,86 +87,101 @@ class DojotAPI():
 
         Returns the created template ID.
         """
-        LOGGER.info("Creating template...")
+        LOGGER.debug("Creating template...")
 
-        template = {
-            "label": "CargoContainer",
-            "attrs": [
-                {
-                    "label": "temperature",
-                    "type": "dynamic",
-                    "value_type": "float"},
-                {
-                    "label": "humidity",
-                    "type": "dynamic",
-                    "value_type": "float"},
-                {
-                    "label": "lightness",
-                    "type": "dynamic",
-                    "value_type": "float"},
-                {
-                    "label" : "gps",
-                    "type" : "dynamic",
-                    "value_type" : "geo:point"}
-            ]
+        args = {
+            "url": "{0}/template".format(CONFIG['dojot']['url']),
+            "headers": {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {0}".format(jwt),
+            },
+            "data": json.dumps({
+                "label": "CargoContainer",
+                "attrs": [
+                    {
+                        "label": "timestamp",
+                        "type": "dynamic",
+                        "value_type": "integer"
+                    },
+                ]
+            }),
         }
 
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer {0}".format(jwt),
+        res = DojotAPI.call_api(requests.post, args)
+
+        LOGGER.debug("... created the template")
+        return res["template"]["id"]
+
+    @staticmethod
+    def create_device(jwt: str, template_id: str, label: str) -> str:
+        """
+        Create a device in Dojot.
+
+        Parameters:
+            jwt: JWT authorization.
+            template_id: template to be used by the device.
+            label: name for the device in Dojot.
+
+        Returns the created device ID.
+        """
+        LOGGER.debug("Creating template...")
+
+        args = {
+            "url": "{0}/device".format(CONFIG['dojot']['url']),
+            "headers": {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {0}".format(jwt),
+            },
+            "data": json.dumps({
+                "templates": [template_id],
+                "attrs": {},
+                "label": label,
+            }),
         }
 
-        res = requests.post(
-            "{0}/template".format(CONFIG['dojot']['url']),
-            data=json.dumps(template),
-            headers=headers
-        )
+        res = DojotAPI.call_api(requests.post, args)
 
-        res.raise_for_status()
-
-        LOGGER.info("... created the template")
-        return json.loads(res.text)["template"]["id"]
+        LOGGER.debug("... created the template")
+        return res["devices"][0]["id"]
 
     @staticmethod
     def delete_devices(jwt: str) -> None:
         """
         Delete all devices.
         """
-        LOGGER.info("Deleting devices...")
+        LOGGER.debug("Deleting devices...")
 
-        headers = {
-            "Authorization": "Bearer {0}".format(jwt),
+        args = {
+            "url": "{0}/device".format(CONFIG['dojot']['url']),
+            "headers": {
+                "Authorization": "Bearer {0}".format(jwt),
+            },
         }
 
-        res = requests.delete(
-            "{0}/device".format(CONFIG['dojot']['url']),
-            headers=headers
-        )
-        res.raise_for_status()
+        DojotAPI.call_api(requests.delete, args)
 
-        LOGGER.info("... deleted devices")
+        LOGGER.debug("... deleted devices")
 
     @staticmethod
     def delete_templates(jwt: str) -> None:
         """
         Delete all templates.
         """
-        LOGGER.info("Deleting templates...")
+        LOGGER.debug("Deleting templates...")
 
-        headers = {
-            "Authorization": "Bearer {0}".format(jwt),
+        args = {
+            "url": "{0}/template".format(CONFIG['dojot']['url']),
+            "headers": {
+                "Authorization": "Bearer {0}".format(jwt),
+            },
         }
 
-        res = requests.delete(
-            "{0}/template".format(CONFIG['dojot']['url']),
-            headers=headers
-        )
-        res.raise_for_status()
+        DojotAPI.call_api(requests.delete, args)
 
-        LOGGER.info("... deleted templates")
+        LOGGER.debug("... deleted templates")
 
     @staticmethod
-    def get_devices(jwt: str) -> list:
+    def get_devices(jwt: str) -> List:
         """
         Retrieves the devices from Dojot.
 
@@ -169,35 +190,34 @@ class DojotAPI():
 
         Returns a list of IDs.
         """
-        LOGGER.info("Retrieving devices...")
+        LOGGER.debug("Retrieving devices...")
 
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer {0}".format(jwt),
+        args = {
+            "url": "{0}/device".format(CONFIG['dojot']['url']),
+            "headers": {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {0}".format(jwt),
+            },
         }
 
-        res = requests.get(
-            "{0}/device".format(CONFIG['dojot']['url']),
-            headers=headers
-        )
-        res.raise_for_status()
+        res = DojotAPI.call_api(requests.get, args)
 
-        devices_ids = [device['id'] for device in json.loads(res.text)['devices']]
+        devices_ids = [device['id'] for device in res['devices']]
 
-        LOGGER.info("... retrieved the devices")
+        LOGGER.debug("... retrieved the devices")
 
         return devices_ids
 
     @staticmethod
-    def divide_loads(n: int, batch: int) -> list:
+    def divide_loads(total: int, batch: int) -> List:
         """
         Divides `n` in a list with each element being up to `batch`.
         """
         loads = []
 
-        if n > batch:
-            iterations = n // batch
-            exceeding = n % batch
+        if total > batch:
+            iterations = total // batch
+            exceeding = total % batch
             # This will create a list with the number `batch` repeated `iterations` times
             # and then `exceeding` at the final
             loads = [batch] * iterations
@@ -205,6 +225,31 @@ class DojotAPI():
                 loads.append(exceeding)
 
         else:
-            loads.append(n)
+            loads.append(total)
 
         return loads
+
+    @staticmethod
+    def call_api(func: Callable[..., requests.Response], args: dict) -> Dict:
+        """
+        Calls the Dojot API using `func` and `args`.
+
+        Parameters:
+            func: function to call Dojot API.
+            args: dictionary of arguments to `func`
+
+        Returns the response in a dictionary
+        """
+        for _ in range(CONFIG['dojot']['api']['retries'] + 1):
+            try:
+                res = func(**args)
+                res.raise_for_status()
+
+            except Exception as exception:
+                LOGGER.debug(str(exception))
+                gevent.sleep(CONFIG['dojot']['api']['time'])
+
+            else:
+                return res.json()
+
+        raise APICallError("exceeded the number of retries to {0}".format(args['url']))
