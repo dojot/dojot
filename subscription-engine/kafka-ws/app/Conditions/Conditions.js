edbaaeb98fa53b18e94a2b6d31af15b0eaaf80ee
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2020-05-05 16:09:10 +0000

    Add conditions that can be applied to the data

---
+++ subscription-engine/kafka-ws/app/Conditions/Conditions.js
@@ -0,0 +1,155 @@
+const omit = require('lodash/omit');
+
+/**
+ * Applies the greater than operator.
+ *
+ * @param {string} parameter parameter that will be analysed
+ * @param {string} value value to be compared
+ * @param {JSON} data the data to be filtered
+ *
+ * @returns {JSON} filtered data.
+ */
+const applyGt = (parameter, value, data) => {
+  if (Number(data[parameter]) > value) {
+    return data;
+  }
+  return omit(data, parameter);
+};
+
+/**
+ * Applies the greater than or equal operator.
+ *
+ * @param {string} parameter parameter that will be analysed
+ * @param {string} value value to be compared
+ * @param {JSON} data the data to be filtered
+ *
+ * @returns {JSON} filtered data.
+ */
+const applyGte = (parameter, value, data) => {
+  if (Number(data[parameter]) >= value) {
+    return data;
+  }
+  return omit(data, parameter);
+};
+
+
+/**
+ * Applies the less than operator.
+ *
+ * @param {string} parameter parameter that will be analysed
+ * @param {string} value value to be compared
+ * @param {JSON} data the data to be filtered
+ *
+ * @returns {JSON} filtered data.
+ */
+const applyLt = (parameter, value, data) => {
+  if (Number(data[parameter]) < value) {
+    return data;
+  }
+  return omit(data, parameter);
+};
+
+/**
+ * Applies the less than or equal operator.
+ *
+ * @param {string} parameter parameter that will be analysed
+ * @param {string} value value to be compared
+ * @param {JSON} data the data to be filtered
+ *
+ * @returns {JSON} filtered data.
+ */
+const applyLte = (parameter, value, data) => {
+  if (Number(data[parameter]) <= value) {
+    return data;
+  }
+  return omit(data, parameter);
+};
+
+
+/**
+ * Applies the equal operator.
+ *
+ * @param {string} parameter parameter that will be analysed
+ * @param {string} value value to be compared
+ * @param {JSON} data the data to be filtered
+ *
+ * @returns {JSON} filtered data.
+ */
+const applyEq = (parameter, value, data) => {
+  if (Number(data[parameter]) === value) {
+    return data;
+  }
+
+  return omit(data, parameter);
+};
+
+/**
+ * Applies the not equal operator.
+ *
+ * @param {string} parameter parameter that will be analysed
+ * @param {string} value value to be compared
+ * @param {JSON} data the data to be filtered
+ *
+ * @returns {JSON} filtered data.
+ */
+const applyNeq = (parameter, value, data) => {
+  if (Number(data[parameter]) !== value) {
+    return data;
+  }
+
+  return omit(data, parameter);
+};
+
+/**
+ * Applies the in operator.
+ *
+ * @param {string} parameter parameter that will be analysed
+ * @param {string[]} values values to be compared
+ * @param {JSON} data the data to be filtered
+ *
+ * @returns {JSON} filtered data.
+ */
+const applyIn = (parameter, values, data) => {
+  for (let i = 0; i < values.length; i += 1) {
+    if (data[parameter].toString().match(values[i])) {
+      return data;
+    }
+  }
+
+  return omit(data, parameter);
+};
+
+/**
+ * Applies the not in operator.
+ *
+ * @param {string} parameter parameter that will be analysed
+ * @param {string[]} values values to be compared
+ * @param {JSON} data the data to be filtered
+ *
+ * @returns {JSON} filtered data.
+ */
+const applyNin = (parameter, values, data) => {
+  let i = 0;
+  for (i = 0; i < values.length; i += 1) {
+    if (data[parameter].toString().match(values[i])) {
+      break;
+    }
+  }
+
+  if (i >= values.length) {
+    return data;
+  }
+
+  return omit(data, parameter);
+};
+
+module.exports = {
+  gt: applyGt,
+  gte: applyGte,
+  lt: applyLt,
+  lte: applyLte,
+  eq: applyEq,
+  neq: applyNeq,
+  in: applyIn,
+  nin: applyNin,
+};
