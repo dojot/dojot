882020514f9b81a3950fda4c80a3bc2e448482d8
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2020-05-05 16:10:04 +0000

    Add ConditionApplier and Condition dir index

---
+++ subscription-engine/kafka-ws/app/Conditions/ConditionApplier.js
@@ -0,0 +1,29 @@
+const flat = require('flat');
+
+const Conditions = require('./Conditions');
+const Utils = require('../Utils');
+
+/**
+ * A condition that can be applied to any given JSON. This is an abstraction to condition functions.
+ *
+ * @param {string} parameter parameter to apply the condition
+ * @param {string} operator operator to be applied
+ * @param {[]} value values to apply the operation
+ *
+ * @returns {(data: JSON) => JSON} a filter function that can be applied to a JSON
+ */
+const ConditionApplier = (parameter, operator, values) => (data) => {
+  let filtered = flat(data);
+
+  // The set operations accept multiple values, so we pass an array
+  if (Utils.setOperators.includes(operator)) {
+    filtered = Conditions[operator](parameter, values, filtered);
+  // The other operations accept only one value, so we pass the first value (a string)
+  } else {
+    filtered = Conditions[operator](parameter, values[0], filtered);
+  }
+
+  return filtered;
+};
+
+module.exports = { ConditionApplier };
