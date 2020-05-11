a7b8dd5d02ef96d6478a63eaa6db52e332858500
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2020-05-11 18:28:28 +0000

    Add better comments about curryRight and mask functions

--- subscription-engine/kafka-ws/app/ProcessingRule/ProcessingRule.js
+++ subscription-engine/kafka-ws/app/ProcessingRule/ProcessingRule.js
@@ -19,6 +19,19 @@ const ProcessingRule = (fields, conditions) => {
    */
 
   // Storing fields filter
+  /*
+   * The `mask` function removes all fields that are not in the variable `fields`. Check json-mask
+   * NPM package for more info.
+   *
+   * The curryRight function transforms the mask function to an arity 1 curried function.
+   * A curried function is a function that receives (ideally) one parameter and returns another
+   * function. Example of a curried function:
+   * let add = (a) => (b) => a + b;
+   * We call it this way:
+   * add(1)(3)
+   * The curryRight function transforms a non-curried function into a curried one, but it inverts
+   * the parameters order, i.e., the last parameter will be the first to be passed.
+   */
   filters.push(curryRight(mask)(fields));
 
   conditions.forEach((condition) => {
