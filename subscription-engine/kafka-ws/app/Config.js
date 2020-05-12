32053d7d1c0dc474269359f91776ad739d8b7fdc
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2020-05-12 19:48:10 +0000

    Add KAFKA_HOSTS environment variable

--- subscription-engine/kafka-ws/app/Config.js
+++ subscription-engine/kafka-ws/app/Config.js
@@ -3,8 +3,7 @@ module.exports = {
     log_level: process.env.LOG_LEVEL || 'info',
   },
   kafka: {
-    host: process.env.KAFKA_HOST || 'kafka',
-    port: parseInt(process.env.KAFKA_PORT, 10) || 9092,
+    host: process.env.KAFKA_HOSTS || 'kafka:9092',
   },
   server: {
     host: process.env.KAFKA_WS_HOST || 'localhost',
