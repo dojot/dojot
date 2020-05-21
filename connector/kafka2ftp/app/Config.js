57a76ce3245a54b8bdb5f7496964568ab6984c01
Mariane Previde <mariane.previde@gmail.com>
2020-05-21 15:00:10 -0300

    parse configs

--- connector/kafka2ftp/app/Config.js
+++ connector/kafka2ftp/app/Config.js
@@ -14,17 +14,17 @@ const config = {
     tenant: process.env.FTP_TENANT || 'admin',
     ftp: {
       host: process.env.FTP_HOST || 'localhost',
-      port: process.env.FTP_PORT || 21,
+      port: parseInt(process.env.FTP_PORT, 10) || 21,
       secure: parseFTPS(process.env.FTP_FTPS || false),
       secureOptions: null,
       user: process.env.FTP_USER || 'anonymous',
       password: process.env.FTP_PASSWORD || 'guest',
-      maxConcurrentConnections: process.env.MAX_CONCURRENT_CONNECTIONS || 10,
+      maxConcurrentConnections: parseInt(process.env.MAX_CONCURRENT_CONNECTIONS, 10) || 10,
       remoteDir: process.env.FTP_REMOTE_DIR || '/',
     },
   }],
   retryOptions: {
-    retries: process.env.RETRIES || 13,
+    retries: parseInt(process.env.RETRIES, 10) || 13,
   },
 };
 
