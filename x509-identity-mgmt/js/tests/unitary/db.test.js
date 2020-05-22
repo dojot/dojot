6669e192e26e8b76a20358bf7ebc1ca70c684349
Thiago de Paula Ferreira <thiagodepaulaferreira@gmail.com>
2020-05-22 16:38:05 -0300

    fixing Codecov issues 6

--- x509-identity-mgmt/js/tests/unitary/db.test.js
+++ x509-identity-mgmt/js/tests/unitary/db.test.js
@@ -1,6 +1,7 @@
 jest.mock('mongoose');
 jest.mock('@dojot/dojot-module-logger');
 
+const EventEmitter = require('events');
 const mongoose = require('mongoose');
 const { logger } = require('@dojot/dojot-module-logger');
 
@@ -14,11 +15,10 @@ const logOnArray = (error) => {
 };
 logger.debug.mockImplementation(logOnArray);
 logger.error.mockImplementation(logOnArray);
+logger.info.mockImplementation(logOnArray);
 
-mongoose.connection = {
-  on: jest.fn(),
-  close: jest.fn(),
-};
+mongoose.connection = new EventEmitter();
+mongoose.connection.close = jest.fn();
 
 const db = require('../../src/db');
 
@@ -32,5 +32,46 @@ describe('testing internal functions of the DB module', () => {
     mongoose.connect.mockRejectedValue(new Error('connection failure'));
     await expect(db.connect()).resolves.toBeUndefined();
     expect(logMessages).toContain('Mongoose connect() failed with error: connection failure');
+  });
+
+  it('log an error', async () => {
+    mongoose.connection.emit('error', new Error('connection failure'));
+    expect(logMessages).toContain('connection failure');
+  });
+
+  it('this should emit events that indicate a healthy connection', async () => {
+    mongoose.connection.emit('connected');
+    expect(db.healthCheck()).toBeTruthy();
+    expect(logMessages).toContain('Connection established to MongoDB');
+
+    mongoose.connection.emit('reconnected');
+    expect(db.healthCheck()).toBeTruthy();
+    expect(logMessages).toContain('Reconnected to MongoDB');
+
+    mongoose.connection.emit('fullsetup');
+    expect(db.healthCheck()).toBeTruthy();
+    expect(logMessages).toContain('Connected to the primary and at least one secondary server on the MongoDB Replica Set');
+
+    mongoose.connection.emit('all');
+    expect(db.healthCheck()).toBeTruthy();
+    expect(logMessages).toContain('Connected to all servers on the MongoDB Replica Set');
+  });
+
+  it('should emit events that indicate connection problems', async () => {
+    jest.useFakeTimers();
+
+    mongoose.connection.emit('disconnected');
+    expect(db.healthCheck()).toBeFalsy();
+    expect(logMessages).toContain('Lost MongoDB connection');
+
+    mongoose.connection.emit('reconnectFailed');
+    expect(db.healthCheck()).toBeFalsy();
+    expect(logMessages).toContain('Reconnected to MongoDB failed');
+
+    mongoose.connection.emit('close');
+    expect(db.healthCheck()).toBeFalsy();
+    expect(logMessages).toContain('MongoDB connection has been closed');
+
+    jest.runOnlyPendingTimers();
   });
 });
