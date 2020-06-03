794657ecec8dbc64c0f15f57fefaeec24fcb3bbe
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2020-06-03 17:53:55 -0300

    Update AgentMessenger to use SDK

--- connector/mqtt/vernemq/v2k-brigde/app/AgentMessenger.js
+++ connector/mqtt/vernemq/v2k-brigde/app/AgentMessenger.js
@@ -1,67 +1,79 @@
-const { IoTAgent } = require('@dojot/iotagent-nodejs');
-const { logger } = require('@dojot/dojot-module-logger');
-const defaultConfig = require('./config');
-const Utils = require('./utils/utils');
+const { Kafka: { Producer }, Logger } = require('@dojot/microservice-sdk');
+const { messenger, producer: sdkConfig, mqtt: mqttConfig } = require('./config');
+const Utils = require('./utils');
 const MQTTClient = require('./MqttClient');
-
-const TAG = { filename: 'AgentMessenger' };
 
 /**
  * Class representing an AgentMessenger
+ *
  * @class
  */
 class AgentMessenger {
   /**
    * Create an agentMessenger
+   *
    * @param {Object} config - the configuration
    */
   constructor(config) {
     this.initialized = false;
-    this.config = config || defaultConfig;
-    this.iotagent = new IoTAgent();
-    this.mqttClient = new MQTTClient(this, this.config);
+    this.producer = new Producer(sdkConfig);
+    this.mqttClient = new MQTTClient(this, config || mqttConfig);
+    this.logger = new Logger('AgentMessenger');
   }
 
   /**
+   * Initialize the agent messenger on success init mqttClient on error exit.
+   *
    * @function init
-   * Initialize the agent messenger
-   * on success init mqttClient
-   * on error exit
    */
   init() {
-    this.iotagent.init().then(() => {
-      logger.debug('... IoT agent was initialized', TAG);
+    this.logger.debug('Initializing Kafka Producer...');
+    this.producer.connect().then(() => {
+      this.logger.debug('... Kafka Producer was initialized');
 
       // initializing mqtt client
-      logger.debug('Initializing MQTTClient', TAG);
+      this.logger.debug('Initializing MQTTClient');
       this.mqttClient.init();
     }).catch(() => {
-      logger.error('An error occurred while initializing the IoTAgent. Bailing out!', TAG);
+      this.logger.error('An error occurred while initializing the Kafka Producer. Bailing out!');
       process.exit(1);
     });
   }
 
   /**
+   * Produce a given message to a given topic.
+   *
    * @function sendMessage
-   * Produce a given message to a given topic
+   *
    * @param {string} topic - topic to produce
    * @param {Object} message - message to produce
    */
   sendMessage(topic, message) {
-    try {
-      logger.debug('Sending message to kafka', TAG);
+    let jsonPayload;
+    let deviceDataMessage;
+    let messageKey;
+    let kafkaTopic;
 
-      const jsonPayload = JSON.parse(message);
-      const deviceDataMessage = Utils.generateDojotDeviceDataMessage(topic, jsonPayload);
-      const messageKey = `${deviceDataMessage.metadata.tenant}:${deviceDataMessage.metadata.deviceid}`;
-      this.iotagent.updateAttrs(deviceDataMessage.metadata.deviceid,
-        deviceDataMessage.metadata.tenant,
-        deviceDataMessage.attrs,
-        deviceDataMessage.metadata,
-        messageKey);
+    try {
+      jsonPayload = JSON.parse(message);
+      deviceDataMessage = Utils.generateDojotDeviceDataMessage(topic, jsonPayload);
+      messageKey = `${deviceDataMessage.metadata.tenant}:${deviceDataMessage.metadata.deviceid}`;
+      kafkaTopic = `${deviceDataMessage.metadata.tenant}.${messenger.kafkaTopic}`;
+      deviceDataMessage = JSON.stringify(deviceDataMessage);
     } catch (error) {
-      logger.error(`Failed to send message to kafka. Error: ${error}`, TAG);
+      this.logger.error(`Failed to create the message. Error: ${error}`);
+      return;
     }
+
+    this.logger.debug('Sending message to kafka');
+    this.producer.produce(kafkaTopic, deviceDataMessage, messageKey).then(() => {
+      this.logger.debug(`Successfully sent message to Kafka in ${kafkaTopic}`);
+    }).catch((error) => {
+      this.logger.error(`Error while sending message to Kafka in ${kafkaTopic}`);
+      if (error) {
+        this.logger.error(error.stack || error);
+      }
+    });
   }
 }
 
--- connector/mqtt/vernemq/v2k-brigde/test/unit/AgentMessenger.test.js
+++ connector/mqtt/vernemq/v2k-brigde/test/unit/AgentMessenger.test.js
@@ -1,20 +1,40 @@
-jest.mock('@dojot/dojot-module');
-jest.mock('@dojot/dojot-module-logger');
-jest.mock('../../app/utils/utils');
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-vars */
+jest.mock('../../app/utils');
 jest.mock('../../app/MqttClient.js');
 
 const mockProcess = require('jest-mock-process');
-const utils = require('../../app/utils/utils');
+const utils = require('../../app/utils');
 const AgentMessenger = require('../../app/AgentMessenger');
 
 const mockExit = mockProcess.mockProcessExit();
 
-/* MOCKS */
+let mockShouldResolve;
+let resolveMock;
+let rejectMock;
+
+// MOCKS
 const mockConfig = {
-  Messenger: {
-    updateAttrs: jest.fn(),
-    init: jest.fn(),
-    on: jest.fn((arg0, arg1, callback) => callback('tenant', 'device')),
+  Producer: {
+    connect: jest.fn(),
+    produce: jest.fn((topic, message, key) => new Promise((resolve, reject) => {
+      resolve = jest.fn(resolve);
+      reject = jest.fn(reject);
+      resolveMock = resolve;
+      rejectMock = reject;
+
+      if (mockShouldResolve) {
+        resolve();
+      } else {
+        reject(new Error('testError'));
+      }
+    })),
+  },
+
+  Logger: {
+    info: jest.fn(),
+    error: jest.fn(),
+    debug: jest.fn(),
   },
 
   kafkaConfig: {
@@ -31,9 +51,6 @@ const mockConfig = {
     mqtt: {
       subscribeTopic: 'topic',
     },
-    app: {
-      logLevel: 'debug',
-    },
   },
 
   mqttConfig: {
@@ -43,19 +60,23 @@ const mockConfig = {
 
 };
 
-jest.mock('@dojot/iotagent-nodejs', () => ({
-  IoTAgent: jest.fn(() => mockConfig.Messenger),
+jest.mock('@dojot/microservice-sdk', () => ({
+  Kafka: {
+    Producer: jest.fn(() => mockConfig.Producer),
+  },
+  Logger: jest.fn(() => mockConfig.Logger),
 }));
 
-let mockedMessenger;
+let mockedProducer;
 
 async function expectConfigs() {
-  mockConfig.Messenger.init.mockReturnValue(Promise.resolve());
+  mockConfig.Producer.connect.mockReturnValue(Promise.resolve());
 
-  await mockedMessenger.init();
+  await mockedProducer.init();
 
-  expect(mockConfig.Messenger.init).toHaveBeenCalled();
-  expect(mockedMessenger.mqttClient.init).toHaveBeenCalled();
+  expect(mockConfig.Producer.connect).toHaveBeenCalled();
+  expect(mockedProducer.mqttClient.init).toHaveBeenCalled();
+  expect(mockedProducer.logger).toBeDefined();
 }
 
 describe('Testing AgentMessenger messenger', () => {
@@ -68,63 +89,85 @@ describe('Testing AgentMessenger messenger', () => {
   });
 
   it('Should init correctly the agent messenger with config and publish data', () => {
-    mockedMessenger = new AgentMessenger(mockConfig.kafkaConfig);
+    mockedProducer = new AgentMessenger(mockConfig.kafkaConfig);
     expectConfigs();
   });
 
   it('Should init correctly the agent messenger without config', () => {
-    mockedMessenger = new AgentMessenger();
+    mockedProducer = new AgentMessenger();
 
     expectConfigs();
   });
 
   it('Should not init correctly the agent messenger', async () => {
     const reason = 'error';
-    mockedMessenger = new AgentMessenger(mockConfig.kafkaConfig);
-    mockConfig.Messenger.init.mockReturnValue(Promise.reject(reason));
+    mockedProducer = new AgentMessenger(mockConfig.kafkaConfig);
+    mockConfig.Producer.connect.mockReturnValue(Promise.reject(reason));
 
     try {
-      await mockedMessenger.init();
+      await mockedProducer.init();
     } catch (error) {
       expect(error).toEqual(reason);
     }
   });
 
-  it('Should send message after onMessage', () => {
-    mockedMessenger = new AgentMessenger(mockConfig.kafkaConfig);
+  it('Should send message', () => {
+    mockedProducer = new AgentMessenger(mockConfig.kafkaConfig);
+    mockShouldResolve = true;
     expectConfigs();
 
-    const generateDataFake = {
+    const generateFakeData = {
       metadata: {
         tenant: 'fake',
         deviceid: 'fake',
       },
     };
-    utils.generateDojotDeviceDataMessage = jest.fn().mockReturnValue(generateDataFake);
+    utils.generateDojotDeviceDataMessage = jest.fn().mockReturnValue(generateFakeData);
 
     const fakeMessage = '{ "name":"John", "age":30, "city":"New York"}';
-    mockedMessenger.sendMessage('test', fakeMessage);
-    expect(mockConfig.Messenger.updateAttrs).toHaveBeenCalled();
+    mockedProducer.sendMessage('test', fakeMessage);
+    expect(mockedProducer.producer.produce).toHaveBeenCalled();
+    expect(resolveMock).toHaveBeenCalled();
   });
 
-
-  it('Should not send message after onMessage', () => {
-    mockedMessenger = new AgentMessenger(mockConfig.kafkaConfig);
+  it('Should not send message', () => {
+    mockedProducer = new AgentMessenger(mockConfig.kafkaConfig);
+    mockShouldResolve = false;
     expectConfigs();
 
-    const generateDataFake = {
+    const generateFakeData = {
       metadata: {
         tenant: 'fake',
         deviceid: 'fake',
       },
     };
-    utils.generateDojotDeviceDataMessage = jest.fn().mockReturnValue(generateDataFake);
+    utils.generateDojotDeviceDataMessage = jest.fn().mockReturnValue(generateFakeData);
+
+    const fakeMessage = '{ "name":"John", "age":30, "city":"New York"}';
+    mockedProducer.sendMessage('test', fakeMessage);
+    expect(mockedProducer.producer.produce).toHaveBeenCalled();
+    expect(rejectMock).toHaveBeenCalled();
+  });
+
+  it('Should not send malformed message', () => {
+    mockedProducer = new AgentMessenger(mockConfig.kafkaConfig);
+    expectConfigs();
+
+    const generateFakeData = {
+      metadata: {
+        tenant: 'fake',
+        deviceid: 'fake',
+      },
+    };
+    utils.generateDojotDeviceDataMessage = jest.fn().mockReturnValue(generateFakeData);
     const fakeMessage = 'error format';
 
     try {
-      mockedMessenger.sendMessage('test', fakeMessage);
+      mockedProducer.sendMessage('test', fakeMessage);
     } catch (error) {
       expect(error).toBeDefined();
+    } finally {
+      expect(mockConfig.Producer.produce).not.toHaveBeenCalled();
     }
   });
 });
