fd08ff572a1547ea9ce61eaf7c75169ac0b22dec
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2020-06-09 11:11:54 -0300

    Add try/catch to AgentMessenger init

--- connector/mqtt/vernemq/v2k-bridge/app/app.js
+++ connector/mqtt/vernemq/v2k-bridge/app/app.js
@@ -1,9 +1,3 @@
-/**
- * @overview Initialize a v2k-client creating an Agent Messenger,
- * then initialize it, also set log level for the service.
- * @author Dojot
- */
-
 const { Logger } = require('@dojot/microservice-sdk');
 
 const { app: appConfig } = require('./config');
@@ -12,7 +6,14 @@ const AgentMessenger = require('./AgentMessenger');
 // Logger configuration
 Logger.setTransport('console', appConfig.logger.transports.console);
 Logger.setVerbose(appConfig.logger.verbose);
+const logger = new Logger('app');
 
 const agentMessenger = new AgentMessenger();
 
-agentMessenger.init();
+try {
+  agentMessenger.init();
+} catch (error) {
+  logger.error('An error occurred while initializing the Agent Messenger. Bailing out!');
+  logger.error(error);
+  process.exit(1);
+}
