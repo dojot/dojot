ca4a71cb1c3f87c55a152274faa1337eddb21246
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2020-06-17 15:54:53 -0300

    Adds new configs and formating in config file

--- connector/mqtt/vernemq/k2v-bridge/app/config.js
+++ connector/mqtt/vernemq/k2v-bridge/app/config.js
@@ -1,32 +1,73 @@
-const hostname = process.env.HOSTNAME || 'k2v-bridge';
-const parseSecureMode = (mode) => ((mode || false) && (mode.toString().toLowerCase().trim() === 'true' || Number(mode) > 0));
+const { toBoolean } = require('./utils');
 
 const app = {
-  logLevel: process.env.K2V_LOG_LEVEL || 'info',
-  baseDir: process.env.BASE_DIR || '/opt/k2v-bridge',
-  hostname,
+  basedir: process.env.K2V_APP_BASEDIR,
+  'connection.retry.count': parseInt(process.env.K2V_APP_CONNECTION_RETRY_COUNT, 10),
+  'connection.retry.timeout': parseInt(process.env.K2V_APP_CONNECTION_RETRY_TIMEOUT, 10),
+  'ejbca.address': process.env.K2V_APP_EJBCA_ADDRESS,
+  hostname: process.env.K2V_APP_HOSTNAME,
+};
+
+const logger = {
+  'transports.console.level': process.env.K2V_LOGGER_TRANSPORTS_CONSOLE_LEVEL,
+  verbose: (toBoolean(process.env.K2V_LOGGER_VERBOSE)),
+};
+
+const messenger = {
+  'consume.topic.suffix': process.env.K2V_MESSENGER_CONSUME_TOPIC_SUFFIX,
 };
 
 const mqtt = {
-  clientUsername: process.env.K2V_MQTT_USERNAME || 'k2v-bridge',
-  clientId: process.env.K2V_MQTT_CLIENT_ID || hostname,
-  host: process.env.K2V_MQTT_HOST || 'vernemq-k8s',
-  port: parseInt(process.env.K2V_MQTT_PORT, 10) || 8883,
-  keepAlive: parseInt(process.env.K2V_MQTT_KEEPALIVE, 10) || 60,
-  secure: parseSecureMode(process.env.K2V_MQTT_SECURE || true),
-  publishTopicSuffix: process.env.K2V_MQTT_PUBLISH_TOPIC_SUFFIX || '/config',
-  publishQos: parseInt(process.env.K2V_MQTT_PUBLISH_QOS, 10) || 1,
-  tls: {
-    ca: {
-      location: process.env.K2V_MQTT_CA_FILE || `${app.baseDir}/app/cert/ca.crt`,
-    },
-    certificate: {
-      location: process.env.K2V_MQTT_CERT_FILE || `${app.baseDir}/app/cert/${hostname}.crt`,
-    },
-    privateKey: {
-      location: process.env.K2V_MQTT_KEY_FILE || `${app.baseDir}/app/cert/${hostname}.key`,
-    },
-  },
+  // Client
+  'client.id': process.env.K2V_MQTT_CLIENT_ID,
+  'client.keepalive': parseInt(process.env.K2V_MQTT_CLIENT_KEEPALIVE, 10),
+  'client.publish.qos': parseInt(process.env.K2V_MQTT_CLIENT_PUBLISH_QOS, 10),
+  'client.publish.topic.suffix': process.env.K2V_MQTT_CLIENT_PUBLISH_TOPIC_SUFFIX,
+  'client.secure': toBoolean(process.env.K2V_MQTT_CLIENT_SECURE),
+  'client.username': process.env.K2V_MQTT_CLIENT_USERNAME,
+
+  // Server
+  'server.address': process.env.K2V_MQTT_SERVER_ADDRESS,
+  'server.port': parseInt(process.env.K2V_MQTT_SERVER_PORT, 10),
+
+  // TLS
+  'tls.ca.file': process.env.K2V_MQTT_TLS_CA_FILE,
+  'tls.certificate.file': process.env.K2V_MQTT_TLS_CERTIFICATE_FILE,
+  'tls.key.file': process.env.K2V_MQTT_TLS_KEY_FILE,
 };
 
-module.exports = { app, mqtt, parseSecureMode };
+const kafka = {
+  acks: parseInt(process.env.K2V_KAFKA_ACKS, 10),
+  'batch.num.messages': parseInt(process.env.K2V_KAFKA_BATCH_NUM_MESSAGES, 10),
+  'client.id': process.env.K2V_KAFKA_CLIENT_ID,
+  'compression.codec': process.env.K2V_KAFKA_COMPRESSION_CODEC,
+  // dr_cb: (toBoolean(process.env.K2V_KAFKA_DR_CB)),
+  'enable.idempotence': (toBoolean(process.env.K2V_KAFKA_ENABLE_IDEMPOTENCE)),
+  'group.id': process.env.K2V_KAFKA_GROUP_ID,
+  'max.in.flight.requests.per.connection':
+    parseInt(process.env.K2V_KAFKA_MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 10),
+  'metadata.broker.list': process.env.K2V_KAFKA_METADATA_BROKER_LIST,
+  retries: parseInt(process.env.K2V_KAFKA_RETRIES, 10),
+  'queue.buffering.max.kbytes': parseInt(process.env.K2V_KAFKA_QUEUE_BUFFERING_MAX_KBYTES, 10),
+  'queue.buffering.max.ms': parseFloat(process.env.K2V_KAFKA_QUEUE_BUFFERING_MAX_MS),
+  'retry.backoff.ms': parseInt(process.env.K2V_KAFKA_RETRY_BACKOFF_MS, 10),
+  'socket.keepalive.enable': (toBoolean(process.env.K2V_KAFKA_SOCKET_KEEPALIVE_ENABLE)),
+};
+
+const sdk = {
+  'commit.interval.ms': parseInt(process.env.K2V_SDK_COMMIT_INTERVAL_MS, 10),
+  'in.processing.max.messages': parseInt(process.env.K2V_SDK_IN_PROCESSING_MAX_MESSAGES, 10),
+  'queued.max.messages.bytes': parseInt(process.env.K2V_SDK_QUEUED_MAX_MESSAGES_BYTES, 10),
+  'subscription.backoff.delta.ms': parseInt(process.env.K2V_SDK_SUBSCRIPTION_BACKOFF_DELTA_MS, 10),
+  'subscription.backoff.max.ms': parseInt(process.env.K2V_SDK_SUBSCRIPTION_BACKOFF_MAX_MS, 10),
+  'subscription.backoff.min.ms': parseInt(process.env.K2V_SDK_SUBSCRIPTION_BACKOFF_MIN_MS, 10),
+};
+
+module.exports = {
+  app,
+  kafka,
+  logger,
+  messenger,
+  mqtt,
+  sdk,
+};
