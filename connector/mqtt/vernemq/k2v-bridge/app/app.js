85f0e1da58dce73cac803ee1557d8b231ca7490e
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2020-06-17 16:05:04 -0300

    Adds logger and error handling in main file

--- connector/mqtt/vernemq/k2v-bridge/app/app.js
+++ connector/mqtt/vernemq/k2v-bridge/app/app.js
@@ -1,15 +1,27 @@
-/**
- * @override Initializes a k2v-bridge creating 'an Agent Messenger
- * then initializes it, also set log level for the service.
- */
+const { Logger } = require('@dojot/microservice-sdk');
 
-const { logger } = require('@dojot/dojot-module-logger');
+const util = require('util');
+const { unflatten } = require('flat');
+
 const config = require('./config');
-const MqttClient = require('./MqttClient');
+const MQTTClient = require('./MqttClient');
 
-const mqttClient = new MqttClient();
+// Logger configuration
+const logConfig = unflatten(config.logger);
+Logger.setTransport('console', logConfig.transports.console);
+Logger.setVerbose(logConfig.verbose);
+const logger = new Logger('app');
 
-/* set log level */
-logger.setLevel(config.app.logLevel);
+logger.info(`Configuration:\n${util.inspect(config, false, 5, true)}`);
 
-mqttClient.init();
+
+
+const mqttClient = new MQTTClient();
+
+try {
+  mqttClient.init();
+} catch (error) {
+  logger.error('An error occurred while initializing the MQTT Client. Bailing out!');
+  logger.error(error.stack || error);
+  process.exit(1);
+}
