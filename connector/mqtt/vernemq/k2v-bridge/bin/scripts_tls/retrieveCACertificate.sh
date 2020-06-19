b7982f9efdf31865af4a49894ececccb533b4bf3
Thiago de Paula Ferreira <thiagodepaulaferreira@gmail.com>
2020-06-19 16:39:53 -0300

    change in x509 certificate request scripts

--- connector/mqtt/vernemq/k2v-bridge/bin/scripts_tls/retrieveCACertificate.sh
+++ connector/mqtt/vernemq/k2v-bridge/bin/scripts_tls/retrieveCACertificate.sh
@@ -8,9 +8,11 @@ BASE_DIR=${BASE_DIR:-"/k2v_bridge"}
 
 . "${BASE_DIR}"/bin/scripts_tls/saveFormattedCRT.sh
 
-echo "Retrieve cert for  ${certCAName} CA : ${certEjbcaApiUrl}/ca/${certCAName}"
-certCa=$(curl --silent -X GET "${certEjbcaApiUrl}"/ca/"${certCAName}" \
--H "Content-Type:application/json" \
--H "Accept:application/json" | jq '.certificate' -r )
+echo
+echo "Retrieve certificate of trusted CA : ${certEjbcaApiUrl}/internal/api/v1/throw-away/ca "
+
+certCa=$(curl -X GET "${certEjbcaApiUrl}"/internal/api/v1/throw-away/ca \
+  -H "Content-Type:application/json" \
+  -H "Accept:application/json" | jq '.caPem' -r )
 
 _saveFormattedCRT "${certDir}/${certCaFile}" "${certCa}"
