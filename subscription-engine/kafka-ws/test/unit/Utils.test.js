5626711d3f7f8d74559273a2f8d25625a6305938
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2020-08-19 16:51:44 -0300

    Remove isObjectEmpty tests

--- subscription-engine/kafka-ws/test/unit/Utils.test.js
+++ subscription-engine/kafka-ws/test/unit/Utils.test.js
@@ -1,5 +1,4 @@
 const {
-  isObjectEmpty,
   checkTopicBelongsTenant,
 } = require('../../app/Utils');
 
@@ -9,20 +8,6 @@ beforeAll(() => {
 });
 beforeEach(() => {
   jest.clearAllMocks();
-});
-
-describe('Testing "isObjectEmpty()"', () => {
-  it('should be an empty object', async () => {
-    expect(isObjectEmpty({})).toBe(true);
-  });
-
-  it('should not be an empty object', async () => {
-    expect(isObjectEmpty({ test: 123 })).toBe(false);
-  });
-
-  it('should not be an empty object if null', async () => {
-    expect(isObjectEmpty(null)).toBe(false);
-  });
 });
 
 describe('Testing "checkTopicBelongsTenant()"', () => {
