28fa0e12a4ee996c1343c4f68f9a27a7d5c31493
codefactor-io <support@codefactor.io>
2020-10-21 20:12:13 +0000

    [CodeFactor] Apply fixes to commit ae11ae2

--- subscription-engine/kafka-ws/index.js
+++ subscription-engine/kafka-ws/index.js
@@ -4,7 +4,7 @@ const https = require('https');
 const { Logger } = require('@dojot/microservice-sdk');
 const { app: appCfg, server: serverCfg } = require('./app/Config');
 
-//test 123
+// test 123
 
 Logger.setTransport('console', {
   level: appCfg.log.log_console_level,
