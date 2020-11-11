1afa2fca658773dbccc0c06c2d0103c90b1f9a58
jonaphael <jonaphael4@gmail.com>
2020-11-11 16:30:23 -0300

    fixed requires changes

--- subscription-engine/kafka-ws/bin/entryPoint.sh
+++ subscription-engine/kafka-ws/bin/entryPoint.sh
@@ -8,22 +8,18 @@ if [ ! -z "${DEBUG+x}" ]; then
     set -x
 fi
 
-export KAKKA_WS_CONNECTION_RETRY_COUNT=${KAFKA_WS_APP_CONNECTION_RETRY_COUNT:-"5"}
-export KAFKA_WS_CONNECTION_RETRY_TIMEOUT=${KAFKA_WS_CONNECTION_RETRY_TIMEOUT:-"3"}
+# KAFKA AND REDIS SHARED VARIABLES
+readonly KAFKA_WS_APP_CONNECTION_RETRY_COUNT=${KAFKA_WS_APP_CONNECTION_RETRY_COUNT:-"5"}
+readonly KAFKA_WS_APP_CONNECTION_RETRY_TIMEOUT=${KAFKA_WS_APP_CONNECTION_RETRY_TIMEOUT:-"3"}
 
-export KAFKA_WS_CONSUMER_METADATA_BROKER_LIST=${KAFKA_WS_CONSUMER_METADATA_BROKER_LIST:-"kafka:9092"}
 
-# Redis parameters
-readonly REDIS_CONN_TIMEOUT=${REDIS_CONN_TIMEOUT:-"180"}
-readonly REDIS_HOST=${REDIS_HOST:-"redis"}
-readonly REDIS_PORT=${REDIS_PORT:-"6379"}
-readonly REDIS_PASSWD=${REDIS_PASSWD:-""}
+readonly KAFKA_WS_CONSUMER_METADATA_BROKER_LIST=${KAFKA_WS_CONSUMER_METADATA_BROKER_LIST:-"kafka:9092"}
 
 has_responded=false
 
 # Split kafka brokers by comma
 readonly KAFKA_BROKERS=${KAFKA_WS_CONSUMER_METADATA_BROKER_LIST//,/ }
-for ((i = 0; (i < ${KAKKA_WS_CONNECTION_RETRY_COUNT}); i++));
+for ((i = 0; (i < ${KAFKA_WS_APP_CONNECTION_RETRY_COUNT}); i++));
 do
     for address in ${KAFKA_BROKERS};
     do
@@ -43,7 +39,7 @@ do
         break
     fi
 
-    sleep ${KAFKA_WS_CONNECTION_RETRY_TIMEOUT}
+    sleep ${KAFKA_WS_APP_CONNECTION_RETRY_TIMEOUT}
 done
 
 if [ "$has_responded" == false ]; then
@@ -53,24 +49,29 @@ fi
 echo -e "Connection established with **${address_splited[0]}** on port **${address_splited[1]}**\n"
 
 # REDIS CONNECTION CHECK
+redis_has_responded=false
 
-# Waiting for redis for at most 3 minutes
-START_TIME=$(date +'%s')
-echo "Waiting for Redis fully start. Host '${REDIS_HOST}', '${REDIS_PORT}'..."
+# Redis parameters
+readonly KAFKA_WS_REDIS_HOST=${REDIS_HOST:-"redis"}
+readonly KAFKA_WS_REDIS_PORT=${REDIS_PORT:-"6379"}
+readonly KAFKA_WS_REDIS_PASSWD=${REDIS_PASSWD:-""}
+
+echo "Waiting for Redis fully start. Host '${KAFKA_WS_REDIS_HOST}', '${KAFKA_WS_REDIS_PORT}'..."
 echo "Try ping Redis... "
-PONG=$(redis-cli -h "${REDIS_HOST}" -p "${REDIS_PORT}" -a "${REDIS_PASSWD}" ping | grep PONG)
-while [ -z "${PONG}" ]; do
-    sleep 3
-    echo "Retry Redis ping... "
-    PONG=$(redis-cli -h "${REDIS_HOST}" -p "${REDIS_PORT}" -a "${REDIS_PASSWD}" ping | grep PONG)
+for ((i = 0; (i < ${KAFKA_WS_APP_CONNECTION_RETRY_COUNT}); i++));
+do
+    PONG=$(redis-cli -h "${KAFKA_WS_REDIS_HOST}" -p "${KAFKA_WS_REDIS_PORT}" -a "${KAFKA_WS_REDIS_PASSWD}" ping | grep PONG)
 
-    ELAPSED_TIME=$(($(date +'%s') - ${START_TIME}))
-    if [ "${ELAPSED_TIME}" -gt "${REDIS_CONN_TIMEOUT}" ]
-    then
-        echo "Redis is taking too long to fully start. Exiting!"
-        exit 1
-    fi
+    [[ ! -z "$PONG" ]] && redis_has_responded=true && break || echo "Retrying .."
+    
+    sleep ${KAFKA_WS_APP_CONNECTION_RETRY_TIMEOUT}
 done
-echo "Redis at host '${REDIS_HOST}', port '${REDIS_PORT}' fully started."
+
+if [ "$redis_has_responded" == false ]; then
+    echo "Redid is not available, exiting ..."
+    exit 1
+fi
+
+echo "Redis at host '${KAFKA_WS_REDIS_HOST}', port '${KAFKA_WS_REDIS_PORT}' fully started."
 
 exec "$@"
