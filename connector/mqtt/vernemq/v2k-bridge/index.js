2262d4999a9828ce8beae2bad773dc7427d08686
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2020-11-18 11:28:20 -0300

    [v2k] Add Health Check and Graceful Shutdown (#1850)

--- connector/mqtt/vernemq/v2k-bridge/index.js
+++ connector/mqtt/vernemq/v2k-bridge/index.js
@@ -1,59 +1,52 @@
-const { ConfigManager, Logger } = require('@dojot/microservice-sdk');
+const {
+  ConfigManager: { getConfig, loadSettings, transformObjectKeys },
+  Logger,
+  ServiceStateManager,
+} = require('@dojot/microservice-sdk');
 
+const camelCase = require('lodash.camelcase');
 const util = require('util');
 
 const AgentMessenger = require('./app/AgentMessenger');
+const MQTTClient = require('./app/MQTTClient');
 
 /*
  * TODO: the idea is to create a default environment variable that will be used internally by the
  * ConfigManager to automatically select the user config file. Since this is not something we care
- * to much now, this will do the trick.
+ * too much now, this will do the trick.
  */
 const userConfigFile = process.env.V2K_APP_USER_CONFIG_FILE || 'production.conf';
 // Creating the configuration
-ConfigManager.loadSettings('V2K', userConfigFile);
-const config = ConfigManager.getConfig('V2K');
+loadSettings('V2K', userConfigFile);
+const config = getConfig('V2K');
 
 // Logger configuration
-Logger.setTransport('console', { level: config.logger['transports.console.level'] });
-Logger.setVerbose(config.logger.verbose);
+Logger.setVerbose(config.log.verbose);
+Logger.setTransport('console', { level: config.log['console.level'] });
+if (config.log['file.enable']) {
+  Logger.setTransport('file', {
+    level: config.log['file.level'],
+    filename: config.log['file.filename'],
+  });
+}
 const logger = new Logger('app');
 
 logger.info(`Configuration:\n${util.inspect(config, false, 5, true)}`);
 
-const unhandledRejections = new Map();
-// the unhandledRejections Map will grow and shrink over time,
-// reflecting rejections that start unhandled and then become handled.
-process.on('unhandledRejection', (reason, promise) => {
-  // The 'unhandledRejection' event is emitted whenever a Promise is rejected and
-  // no error handler is attached to the promise within a turn of the event loop.
-  logger.error(`Unhandled Rejection at: ${reason.stack || reason}.`);
-  unhandledRejections.set(promise, reason);
-  logger.debug(`unhandledRejection: List of Unhandled Rejection size ${unhandledRejections.size}`);
+const manager = new ServiceStateManager({
+  lightship: transformObjectKeys(config.lightship, camelCase),
 });
-process.on('rejectionHandled', (promise) => {
-  // The 'rejectionHandled' event is emitted whenever a Promise has
-  // been rejected and an error handler was attached to it
-  // later than one turn of the Node.js event loop.
-  logger.debug('rejectionHandled: A event');
-  unhandledRejections.delete(promise);
-  logger.debug(`rejectionHandled: List of Unhandled Rejection size ${unhandledRejections.size}`);
-});
-
-process.on('uncaughtException', async (ex) => {
-  // The 'uncaughtException' event is emitted when an uncaught JavaScript
-  // exception bubbles all the way back to the event loop.
-  logger.error(`uncaughtException: Unhandled Exception at: ${ex.stack || ex}. Bailing out!!`);
-  // TODO: stop server (connection redis, kafka consumer, etc.)
-  process.exit(1);
-});
-
 const agentMessenger = new AgentMessenger();
+const mqttClient = new MQTTClient(agentMessenger, manager);
 
-try {
-  agentMessenger.init();
-} catch (error) {
-  logger.error('An error occurred while initializing the Agent Messenger. Bailing out!');
-  logger.error(error.stack || error);
-  process.exit(1);
-}
+manager.registerService('kafka');
+manager.addHealthChecker(
+  'kafka',
+  agentMessenger.healthChecker.bind(agentMessenger),
+  config.healthcheck['kafka.interval.ms'],
+);
+
+manager.registerShutdownHandler(agentMessenger.shutdownHandler.bind(agentMessenger));
+manager.registerShutdownHandler(mqttClient.shutdownHandler.bind(mqttClient));
+
+agentMessenger.init(mqttClient);
