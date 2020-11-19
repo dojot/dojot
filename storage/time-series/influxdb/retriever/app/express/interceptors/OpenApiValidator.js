311dcc13f9ceeb0720aafa036c85d46838577ec0
Mariane Previde <mariane.previde@gmail.com>
2020-11-19 19:34:09 -0300

    fix todo

--- storage/time-series/influxdb/retriever/app/express/interceptors/OpenApiValidator.js
+++ storage/time-series/influxdb/retriever/app/express/interceptors/OpenApiValidator.js
@@ -1,16 +1,13 @@
 
 const OpenApiValidator = require('express-openapi-validator');
-const path = require('path');
-
-const apiSpecTemp = path.join(__dirname, '../../../api/swagger.yml');
 
 /**
  * Middleware to validate request/response based in openApi 3.0
  */
-module.exports = (openApiPath) => ({
+module.exports = ({ openApiPath }) => ({
   name: 'validator-interceptor',
   middleware: OpenApiValidator.middleware({
-    apiSpec: apiSpecTemp, // TODO: Check why doest work with openApiPath?
+    apiSpec: openApiPath,
     validateRequests: true,
     validateResponses: true,
   }),
