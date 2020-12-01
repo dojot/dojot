235a3472ad4a56b5b44d5fddc60badd2033749df
Mariane Previde <mariane.previde@gmail.com>
2020-12-01 14:24:00 -0300

    check if already onboard

--- storage/time-series/influxdb/example/init-influx.sh
+++ storage/time-series/influxdb/example/init-influx.sh
@@ -9,15 +9,30 @@ DEFAULT_TOKEN=${DEFAULT_TOKEN:-"dojot@token_default"}
 DEFAULT_ORGANIZATION=${DEFAULT_ORGANIZATION:-"admin"}
 DEFAULT_BUCKET=${DEFAULT_BUCKET:-"dojot"}
 DEFAULT_RETENTION=${DEFAULT_RETENTION:-"14400s"}
+# Valid --retention units are nanoseconds (ns), microseconds (us or µs), milliseconds (ms), seconds (s), minutes (m), hours (h), days (d), and weeks (w).
 
-influx setup \
-    --force \
-    --host "$HOST"  \
-    --username "$DEFAULT_USER" \
-    --password "$DEFAULT_PASSWORD" \
-    --org "$DEFAULT_ORGANIZATION" \
-    --bucket "$DEFAULT_BUCKET" \
-    --token  "$DEFAULT_TOKEN" \
-    --retention "$DEFAULT_RETENTION"
+apt-get install curl jq -y
+
+allowedOnBoard=$(curl  -sS -X GET \
+--url "${HOST}"/api/v2/setup | jq '.allowed')
+
+if [[ "$allowedOnBoard" == "true" ]]; then
+
+    influx setup \
+        --force \
+        --host "$HOST"  \
+        --username "$DEFAULT_USER" \
+        --password "$DEFAULT_PASSWORD" \
+        --org "$DEFAULT_ORGANIZATION" \
+        --bucket "$DEFAULT_BUCKET" \
+        --token  "$DEFAULT_TOKEN" \
+        --retention "$DEFAULT_RETENTION"
+
+    echo "Successfully initialized InfluxDB..."
+
+else
+    echo "Already onboarding"
+fi
+
 
 exit 0
