b0071193856d4da1f69a9158146c19fcfcec5458
Thiago de Paula Ferreira <thiagodepaulaferreira@gmail.com>
2020-12-04 11:34:52 -0300

    Removing comments about the old license

--- x509-identity-mgmt/ejbca/bin/before-shutdown.sh
+++ x509-identity-mgmt/ejbca/bin/before-shutdown.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-##################################################################
-#                                                                #
-# Copyright (c) 2018-2019 PrimeKey Solutions AB.                 #
-#                                                                #
-# This software is free software; you can redistribute it and/or #
-# modify it under the terms of the GNU Lesser General Public     #
-# License as published by the Free Software Foundation; either   #
-# version 2.1 of the License, or any later version.              #
-#                                                                #
-# See terms of license at gnu.org.                               #
-#                                                                #
-##################################################################
-
 baseDir="$1"
 tempDir="$2"
 
@@ -22,12 +9,6 @@ if [ -f ${baseDir}/bin/internal/functions-ejbca ] ; then source ${baseDir}/bin/i
 ejbca_health_check_set_not_ready "true"
 
 
-
-
-
-##################################################################
-# ------------ Customization for the Dojot platform ------------ #
-##################################################################
 # Removes the lock file if the container is interrupted in the
 # middle of initialization...
 # At this point the variable $LOCK_FILE is not accessible,
