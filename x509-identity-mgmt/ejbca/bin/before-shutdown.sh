aa5b74e43c61f760eeb381e9caf599a245bc7e65
Thiago de Paula Ferreira <thiagodepaulaferreira@gmail.com>
2020-12-28 16:46:10 -0300

    license change

--- x509-identity-mgmt/ejbca/bin/before-shutdown.sh
+++ x509-identity-mgmt/ejbca/bin/before-shutdown.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-##################################################################
-#                                                                #
-# Copyright (c) 2018-2019 PrimeKey Solutions AB.                 #
-#                                                                #
-# This software is free software; you can redistribute it and/or #
-# modify it under the terms of the GNU Lesser General Public     #
-# License as published by the Free Software Foundation; either   #
-# version 2.1 of the License, or any later version.              #
-#                                                                #
-# See terms of license at gnu.org.                               #
-#                                                                #
-##################################################################
-
 baseDir="$1"
 tempDir="$2"
 
--- x509-identity-mgmt/ejbca/bin/dojot-custom/configuration-variables.sh
+++ x509-identity-mgmt/ejbca/bin/dojot-custom/configuration-variables.sh
@@ -2,17 +2,6 @@
 
 ##################################################################
 #                                                                #
-# Copyright (c) 2020 Dojot IoT Platform                          #
-#                                                                #
-# This software is free software; you can redistribute it and/or #
-# modify it under the terms of the GNU Lesser General Public     #
-# License as published by the Free Software Foundation; either   #
-# version 2.1 of the License, or any later version.              #
-#                                                                #
-# See terms of license at gnu.org.                               #
-#                                                                #
-# ---------------------------------------------------------------#
-#                                                                #
 # This file defines the variables used in the configuration of   #
 # the customized EJBCA for the Dojot platform. this file is used #
 # in conjunction with the other shell scripts and therefore it   #
