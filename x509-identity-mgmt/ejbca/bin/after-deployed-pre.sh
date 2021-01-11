7a9860c920de7af9e54d3491a73162ad850ab860
Thiago P. Ferreira <thiagodepaulaferreira@gmail.com>
2021-01-11 17:08:27 -0300

    [x509-identity-mgmt] license and version change (#1922)
    * license change
    
    * keeping the original license
    
    * by license coherence
    
    * License notice

--- x509-identity-mgmt/ejbca/bin/after-deployed-pre.sh
+++ x509-identity-mgmt/ejbca/bin/after-deployed-pre.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-##################################################################
-#                                                                #
-# Copyright (c) 2020 Dojot IoT Platform                          #
-#                                                                #
-# This software is free software; you can redistribute it and/or #
-# modify it under the terms of the GNU Lesser General Public     #
-# License as published by the Free Software Foundation; either   #
-# version 2.1 of the License, or any later version.              #
-#                                                                #
-# See terms of license at gnu.org.                               #
-#                                                                #
-##################################################################
-
 baseDir="$1"
 dojotExec="${baseDir}/bin/internal/dojot-custom/exec.sh"
 chmod u+x "${dojotExec}"
--- x509-identity-mgmt/ejbca/bin/dojot-custom/import-profiles.sh
+++ x509-identity-mgmt/ejbca/bin/dojot-custom/import-profiles.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-##################################################################
-#                                                                #
-# Copyright (c) 2020 Dojot IoT Platform                          #
-#                                                                #
-# This software is free software; you can redistribute it and/or #
-# modify it under the terms of the GNU Lesser General Public     #
-# License as published by the Free Software Foundation; either   #
-# version 2.1 of the License, or any later version.              #
-#                                                                #
-# See terms of license at gnu.org.                               #
-#                                                                #
-##################################################################
-
 function importProfiles() {
     # import into EJBCA the certificate profiles, as long as
     # the profile directory exists and has files there!
