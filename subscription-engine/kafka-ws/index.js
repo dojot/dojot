e96fff0852d80c066a919d68258d6af2c5377a83
Jonaphael Aristil <jonaphael4@gmail.com>
2021-02-08 14:19:17 -0300

    remove terminus (#1974)

--- subscription-engine/kafka-ws/index.js
+++ subscription-engine/kafka-ws/index.js
@@ -5,18 +5,14 @@ const util = require('util');
 
 const { ConfigManager, Logger } = require('@dojot/microservice-sdk');
 
-// Loading the configurations with the configManager
 const KAFKA_WS_CONFIG_LABEL = 'KAFKA_WS';
-
 const userConfigFile = process.env.KAFKA_WS_APP_USER_CONFIG_FILE || 'production.conf';
-
 ConfigManager.loadSettings(KAFKA_WS_CONFIG_LABEL, userConfigFile);
 
 const config = ConfigManager.getConfig(KAFKA_WS_CONFIG_LABEL);
 
 const application = require('./app/App');
 const websocketTarball = require('./app/WebsocketTarball');
-const terminus = require('./app/Terminus');
 const StateManager = require('./app/StateManager');
 
 Logger.setTransport('console', { level: config.log['console.level'] });
@@ -73,9 +69,6 @@ server.listen(config.server.port, config.server.host, async () => {
 server.on('close', () => {
   StateManager.signalNotReady(stateService);
 });
-
-/* adds health checks and graceful shutdown to the application */
-terminus.setup(server);
 
 const unhandledRejections = new Map();
 
