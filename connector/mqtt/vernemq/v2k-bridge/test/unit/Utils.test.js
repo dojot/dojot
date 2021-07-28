1afc24536efbe9b371b1729c0bfaa219b50050b6
marciofunes <ubuntu@ubuntu1804.linuxvmimages.local>
2021-07-28 18:20:27 -0400

    increase coverage threshold for branches

--- connector/mqtt/vernemq/v2k-bridge/test/unit/Utils.test.js
+++ connector/mqtt/vernemq/v2k-bridge/test/unit/Utils.test.js
@@ -7,8 +7,31 @@ describe('Utils', () => {
 
   describe('generateDojotDeviceDataMessage', () => {
     it('Should correctly generate the payload', () => {
+      const timestamp_before = Date.now();
       const topic = 'admin:deviceid/topic';
       const payload = 'data';
+      const data = utils.generateDojotDeviceDataMessage(topic, payload);
+      const timestamp_after = Date.now();
+
+      const { deviceid } = data.metadata;
+      const { tenant } = data.metadata;
+      const { attrs } = data;
+
+      const timestampMatch = timestamp_before <= data.metadata.timestamp && timestamp_after >= data.metadata.timestamp;
+
+      expect(deviceid).toEqual('deviceid');
+      expect(tenant).toEqual('admin');
+      expect(attrs).toEqual(payload);
+      expect(timestampMatch).toBe(true);
+
+    });
+  });
+
+  describe('generateDojotDeviceDataMessage', () => {
+    it('Should correctly generate the payload', () => {
+      const timestampFake = 1605093071000;
+      const topic = 'admin:deviceid/topic';
+      const payload = '{"temperatura": 10, "timestamp": 1605093071000}';
       const data = utils.generateDojotDeviceDataMessage(topic, payload);
 
       const { deviceid } = data.metadata;
@@ -18,7 +41,27 @@ describe('Utils', () => {
       expect(deviceid).toEqual('deviceid');
       expect(tenant).toEqual('admin');
       expect(attrs).toEqual(payload);
-      expect(payload.timestamp).toEqual('2020-05-05T05:00:00.000000Z');
+      expect(data.metadata.timestamp).toEqual(1605093071000);
+
+    });
+  });
+
+  describe('generateDojotDeviceDataMessage', () => {
+    it('Should correctly generate the payload', () => {
+      const timestampFake = "2020-05-05T05:00:00.000000Z";
+      const topic = 'admin:deviceid/topic';
+      const payload = '{"temperatura": 10, "timestamp": "2020-05-05T05:00:00.000000Z"}';
+      const data = utils.generateDojotDeviceDataMessage(topic, payload);
+
+      const { deviceid } = data.metadata;
+      const { tenant } = data.metadata;
+      const { attrs } = data;
+
+      expect(deviceid).toEqual('deviceid');
+      expect(tenant).toEqual('admin');
+      expect(attrs).toEqual(payload);
+      expect(data.metadata.timestamp).toEqual("2020-05-05T05:00:00.000000Z");
+
     });
   });
 
