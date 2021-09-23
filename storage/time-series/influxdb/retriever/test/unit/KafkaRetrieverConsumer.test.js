641c4656a9f2e9b67f78dfac3afb2035a7168d3f
Rodrigo A. S. Maia <ramaia@cpqd.com.br>
2021-09-23 16:16:26 -0300

    Add: Retriever consumer tests

---
+++ storage/time-series/influxdb/retriever/test/unit/KafkaRetrieverConsumer.test.js
@@ -0,0 +1,197 @@
+const mockConfig = {
+  lightship: { a: 'abc' },
+  graphql: { graphiql: true },
+  subscribe: {
+    'topics.suffix.tenants': 'tenancy.topic',
+    'topics.suffix.device.manager': 'device.topic',
+  },
+};
+
+const mockSdk = {
+  ConfigManager: {
+    getConfig: jest.fn(() => mockConfig),
+    transformObjectKeys: jest.fn((obj) => obj),
+  },
+  Kafka: {
+    Consumer: jest.fn().mockImplementation(() => ({
+      init: jest.fn(),
+      // eslint-disable-next-line no-unused-vars
+      registerCallback: jest.fn((topic, callback) => topic),
+      getStatus: jest.fn(() => ({
+        connected: true,
+      })),
+      finish: jest.fn(),
+    })),
+  },
+  Logger: jest.fn(() => ({
+    debug: jest.fn(),
+    error: jest.fn(),
+    info: jest.fn(),
+    warn: jest.fn(),
+  })),
+};
+
+jest.mock('@dojot/microservice-sdk', () => mockSdk);
+
+jest.mock('../../app/lib/localPersistence/index', () => ({
+  InputPersister: jest.fn().mockImplementation(() => ({
+    dispatch: jest.fn(() => Promise.resolve()),
+  })),
+  InputPersisterArgs: {
+    INSERT_OPERATION: 'put',
+  },
+}));
+
+const RetrieverConsumer = require('../../app/kafka/RetrieverConsumer');
+
+describe('RetrieverConsumer', () => {
+  let retrieverConsumer;
+  beforeEach(() => {
+    const mockLocalPersistence = {};
+    retrieverConsumer = new RetrieverConsumer(mockLocalPersistence);
+  });
+
+  it('Should init the consumer ', async () => {
+    let error;
+    try {
+      await retrieverConsumer.init();
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).toBeUndefined();
+  });
+
+  it('Should throw an error when an error happened ', async () => {
+    let error;
+
+    retrieverConsumer.consumer.init = jest.fn(() => { throw new Error('error'); });
+
+    try {
+      await retrieverConsumer.init();
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).toBeDefined();
+  });
+
+  it('Should init the consumer of the tenancy topic', async () => {
+    let error;
+    retrieverConsumer.getCallbackForNewTenantEvents = jest.fn(() => () => {});
+
+    try {
+      await retrieverConsumer.initCallbackForNewTenantEvents();
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).toBeUndefined();
+  });
+
+  it('Should return a callback to hear the tenancy topic', () => {
+    let error;
+    retrieverConsumer.registerCallbacksForDeviceEvents = jest.fn();
+
+    const data = {
+      value: '{"type": "CREATE"}',
+    };
+    const ack = () => {};
+    expect.assertions(1);
+    try {
+      const callback = retrieverConsumer.getCallbackForNewTenantEvents();
+      callback(data, ack);
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).toBeUndefined();
+  });
+
+  it('Should init the consumer of the devices topic', async () => {
+    let error;
+    retrieverConsumer.getCallbacksForDeviceEvents = jest.fn(() => () => {});
+    try {
+      await retrieverConsumer.initCallbackForDeviceEvents('tenant');
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).toBeUndefined();
+  });
+
+  it('Should return a callback to hear the devices topic', () => {
+    let error;
+    retrieverConsumer.registerCallbacksForDeviceEvents = jest.fn();
+
+    const data = {
+      value: '{"event": "create"}',
+    };
+    const ack = () => {};
+    expect.assertions(1);
+    try {
+      const callback = retrieverConsumer.getCallbacksForDeviceEvents();
+      callback(data, ack);
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).toBeUndefined();
+  });
+
+  it('Should return true, when connection is active ', async () => {
+    const active = await retrieverConsumer.isConnected();
+    expect(active).toEqual(true);
+  });
+
+  it('Should return false, when connection is disabled ', async () => {
+    retrieverConsumer.consumer.getStatus = jest.fn(() => ({
+      connected: false,
+    }));
+
+    const active = await retrieverConsumer.isConnected();
+    expect(active).toEqual(false);
+  });
+
+  it('Should return false, when an error happened', async () => {
+    retrieverConsumer.consumer.getStatus = jest.fn(() => {
+      throw new Error('Error');
+    });
+
+    const active = await retrieverConsumer.isConnected();
+    expect(active).toEqual(false);
+  });
+
+  it('Should finish kafka consumer', async () => {
+    await retrieverConsumer.finish();
+
+    expect(retrieverConsumer.consumer).toBeNull();
+  });
+
+  it('Should unregister callbacks in kafka consumer', async () => {
+    retrieverConsumer.consumer.unregisterCallback = jest.fn();
+    retrieverConsumer.idCallbackTenant = 1;
+    retrieverConsumer.idCallbackDeviceManager = 1;
+
+    await retrieverConsumer.unregisterCallbacks();
+
+    expect(retrieverConsumer.idCallbackTenant).toBeNull();
+    expect(retrieverConsumer.idCallbackDeviceManager).toBeNull();
+  });
+
+  it('Should handle the error, when an error happened', async () => {
+    let error;
+
+    retrieverConsumer.consumer.unregisterCallback = jest.fn(() => { throw new Error('error'); });
+    retrieverConsumer.idCallbackTenant = 1;
+    retrieverConsumer.idCallbackDeviceManager = 1;
+
+    try {
+      await retrieverConsumer.unregisterCallbacks();
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).toBeDefined();
+  });
+});
