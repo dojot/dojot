6bcd768aed16379c9638d9255e22fc2c57db8ae9
Rodrigo A. S. Maia <ramaia@cpqd.com.br>
2021-11-03 13:57:31 -0300

    [file-mgmt] Update: readme and index

--- storage/file/minio/file-mgmt/src/index.js
+++ storage/file/minio/file-mgmt/src/index.js
@@ -9,8 +9,8 @@ const App = require('./app/app');
 Logger.setLevel('console', 'debug');
 const logger = new Logger('file-mgmt:Server');
 const openApiPath = path.join(__dirname, '../docs/v1.yml');
-ConfigManager.loadSettings('FILE-MGMT', 'default.conf');
-const config = ConfigManager.getConfig('FILE-MGMT');
+ConfigManager.loadSettings('FILEMGMT', 'default.conf');
+const config = ConfigManager.getConfig('FILEMGMT');
 
 // Init Application
 const app = new App(config, logger, openApiPath);
