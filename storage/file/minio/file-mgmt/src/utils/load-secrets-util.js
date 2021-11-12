96c084841e3e1a7bd0e1e2a9ee67f9981810980f
Rodrigo A. S. Maia <ramaia@cpqd.com.br>
2021-11-12 14:07:57 -0300

    [file-mgmt ] Fix: Code factor

--- storage/file/minio/file-mgmt/src/utils/load-secrets-util.js
+++ storage/file/minio/file-mgmt/src/utils/load-secrets-util.js
@@ -3,11 +3,15 @@ const fs = require('fs');
 
 async function loadSecrets(config) {
   if (config.minio['access.key.file']) {
+    // It is necessary to read a file in an absolute path   
+    // eslint-disable-next-line security/detect-non-literal-fs-filename
     const accessKey = await fs.promises.readFile(`/secrets/${config.minio['access.key.file']}`, 'utf-8');
     config.minio['access.key'] = accessKey.replace('\n', '');
   }
   
   if (config.minio['secret.key.file']) {
+    // It is necessary to read a file in an absolute path
+    // eslint-disable-next-line security/detect-non-literal-fs-filename
     const secretKey = await fs.promises.readFile(`/secrets/${config.minio['secret.key.file']}`, 'utf-8');
     config.minio['secret.key'] = secretKey.replace('\n', '');
   }
