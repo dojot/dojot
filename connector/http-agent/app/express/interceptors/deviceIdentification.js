4f4c8db739f8072d4fcf4f655a15bd512f8d3d7a
Gilmaicor Leandro <gilmaicor@hotmail.com>
2021-11-25 12:05:30 -0300

    fix: review comments

--- connector/http-agent/app/express/interceptors/deviceIdentification.js
+++ connector/http-agent/app/express/interceptors/deviceIdentification.js
@@ -2,6 +2,9 @@ const createError = require('http-errors');
 const axios = require('axios');
 const tls = require('tls');
 
+const Forbidden = new createError[403]();
+const BadRequest = new createError[400]();
+
 /**
  * A middleware to get device identification from the certificate
  *
@@ -14,39 +17,29 @@ const tls = require('tls');
 module.exports = ({ cache, config }) => ({
   name: 'device-identification-interceptor',
   middleware: async (req, res, next) => {
-    const Forbidden = new createError[403]();
-    const BadRequest = new createError[400]();
-
     if (req.socket instanceof tls.TLSSocket) {
       const clientCert = req.socket.getPeerCertificate();
-      if (
-        config.authorizationMode === 'cn' &&
-        Object.prototype.hasOwnProperty.call(clientCert, 'subject') &&
-        Object.hasOwnProperty.bind(clientCert.subject)('CN')
-      ) {
-        const { CN } = clientCert.subject;
-        try {
+      try {
+        if (
+          config.authorizationMode === 'cn' &&
+          Object.prototype.hasOwnProperty.call(clientCert, 'subject') &&
+          Object.hasOwnProperty.bind(clientCert.subject)('CN')
+        ) {
+          const { CN } = clientCert.subject;
           const messageKey = CN.split(':');
           [req.tenant, req.deviceId] = messageKey;
           return next();
-        } catch (e) {
-          Forbidden.message =
-            'Error trying to get tenant and deviceId in CN of certificate.';
-          return next(Forbidden);
         }
-      }
-
-      if (
-        config.authorizationMode === 'fingerprint' &&
-        Object.prototype.hasOwnProperty.call(clientCert, 'fingerprint256')
-      ) {
-        const { fingerprint256 } = clientCert;
-        let messageKey = cache.get(fingerprint256);
-        if (messageKey) {
-          [req.tenant, req.deviceId] = messageKey;
-          return next();
-        }
-        try {
+        if (
+          config.authorizationMode === 'fingerprint' &&
+          Object.prototype.hasOwnProperty.call(clientCert, 'fingerprint256')
+        ) {
+          const { fingerprint256 } = clientCert;
+          let messageKey = cache.get(fingerprint256);
+          if (messageKey) {
+            [req.tenant, req.deviceId] = messageKey;
+            return next();
+          }
           messageKey = await axios
             .get(
               `http://certificate-acl:3000/internal/api/v1/acl-entries/${fingerprint256}`,
@@ -55,15 +48,11 @@ module.exports = ({ cache, config }) => ({
           [req.tenant, req.deviceId] = messageKey;
           cache.set(fingerprint256, messageKey, config.setTll);
           return next();
-        } catch (e) {
-          Forbidden.message =
-            'Error trying to get tenant and deviceId in certificate-acl.';
-          return next(Forbidden);
         }
+      } catch (err) {
+        Forbidden.message = `Client certificate is invalid: ${err.message}`;
+        return next(Forbidden);
       }
-
-      Forbidden.message = 'Client certificate is invalid';
-      return next(Forbidden);
     }
 
     const reqType = req.path.split('/');
