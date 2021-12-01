24fc71971240a6906cd576ab03a037a112d76c66
codefactor-io <support@codefactor.io>
2021-12-01 18:56:26 +0000

    [CodeFactor] Apply fixes to commit e30e66f

--- iam/device-basic-authentication/app/kafka/ConsumerMessages.js
+++ iam/device-basic-authentication/app/kafka/ConsumerMessages.js
@@ -91,7 +91,7 @@ class ConsumerMessages {
     const topic = RegExp(configSubscribe['topics.regex.tenants']);
 
     this.idCallbackTenant = this.consumer.registerCallback(
-      topic, 
+      topic,
       this.getCallbackForTenantEvents(),
     );
     logger.debug('registerCallback: Registered Callback');
